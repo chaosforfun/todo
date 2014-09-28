47c51c6c137158d604a86f034f66e19994693e46
zhaoshichao <zhaoshichao@adsage.com>
2014-09-28 18:55:31 +0800

    initial

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zhaoshichao on 14-9-28.
+ */
+/*
+* 程序入口
+* */
+var todo = angular.module('todo', []);
---
+++ app/js/controller/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zhaoshichao on 14-9-28.
+ */
+/*
+* 主页面
+* */
+todo.controller('main', ['$scope', function ($scope) {
+
+}]);
