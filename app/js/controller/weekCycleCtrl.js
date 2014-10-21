5760441c8082066e1409ceffdddc4502597b9d65
zhaoshichao <zhaoshichao@adsage.com>
2014-10-21 21:41:36 +0800

    构建一周

--- app/js/controller/weekCycleCtrl.js
+++ app/js/controller/weekCycleCtrl.js
@@ -1,2 +1,20 @@
-todo.controller('weekCycleCtrl', ['$scope', 
-	function($scope) {}])
+todo.controller('weekCycleCtrl', ['$scope',
+    function ($scope) {
+        function weekFactory(day) {
+            var day = day || new Date();
+            var week = [];
+            var i = 1,
+                l = day.getDay();
+            var oneDay = 1000*3600*24;
+            //构建day 之前的日期
+            for (;i <= l; i++) {
+                week.push(new Date(day.getTime() - oneDay*(l - i)));
+            }
+            //构建day之后的日期
+            while(week.length !== 7) {
+                week.push(new Date(day.getTime() + oneDay*(week.length - l + 1)));
+            }
+            return week;
+        }
+        $scope.week = weekFactory();
+    }]);
---
+++ app/js/filter/filter.js
@@ -0,0 +1,24 @@
+/**
+ * Created by zhaoshichao on 14-10-21.
+ */
+todo.filter('i8n', function () {
+    return function(input) {
+        switch (input.getDay()) {
+            case 1:
+                return '周一';
+            case 2:
+                return '周二';
+            case 3:
+                return '周三';
+            case 4:
+                return '周四';
+            case 5:
+                return '周五';
+            case 6:
+                return '周六';
+            case 0:
+                return '周日';
+        }
+        return input.getDay()
+    }
+});
